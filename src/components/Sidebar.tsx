b65db2d9540f5da3baee46f149fa870837947521
francisco santos <francisco.santos@zallpy.com>
2023-04-14 11:15:24 -0300

    add style sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,49 @@
+import { gql, useQuery } from "@apollo/client";
+import { Lesson } from "./Lesson";
+
+const GET_LESSON_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      lessonType
+      availableAt
+      title
+      slug
+    }
+  }
+`;
+
+interface GetLessonsQueryResponse {
+  lessons: {
+    id: string
+    title: string
+    slug: string
+    availableAt: string
+    LessonType: 'live' | 'class'
+  }[]
+
+}
+
 export function Sidebar () {
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSON_QUERY)
+
   return (
-    <h1>Sidebar</h1>
+    <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+        Cronogramas de aulas
+      </span>
+
+      <div className="flex flex-col gap-8">
+        {data?.lessons.map(lesson => (
+          <Lesson
+            key={lesson.id}
+            title={lesson.title} 
+            type={lesson.LessonType}
+            availableAt={new Date(lesson.availableAt)}
+            slug={lesson.slug}
+          />
+        ))}
+      </div>
+    </aside>
   );
 }
